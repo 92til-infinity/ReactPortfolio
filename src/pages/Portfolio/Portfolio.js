49846140a8112b0fee5347e9943d922ad05bb3d0
Scott Glover <glover.scott92@gmail.com>
2020-09-24 15:24:28 -0700

    testing

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -12,77 +12,88 @@ class Projects extends Component {
 
         if (this.state.activeTab === 0) {
             return (
-                <div className="projects-grid">
-                    {/* Project 1 */}
-                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
-                        <CardText>
-                            Happy Hour Project
+                <div><h1>This is React</h1>
+                    <div className="projects-grid">
+                        <br></br><br></br>
+                        {/* Project 1 */}
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
+                            <CardText>
+                                Study Check
+                        </CardText>
+                            <CardActions border>
+                                <Button colored href="https://github.com/92til-infinity/Project3.333">GitHub</Button>
+                                <Button colored href="http://studycheck.com">Heroku</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        {/* Project 2 */}
+                        {/* <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #2</CardTitle>
+                            <CardText>
+                                2Play Project
                                     </CardText>
-                        <CardActions border>
-                            <Button colored href="https://92til-infinity.github.io/UCDBC-HappyHour/">GitHub</Button>
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                            <CardActions border>
+                                <Button colored href="https://github.com/92til-infinity/Project2x4">GitHub</Button>
 
-                    {/* Project 2 */}
-                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #2</CardTitle>
-                        <CardText>
-                            2Play Project
-                                    </CardText>
-                        <CardActions border>
-                            <Button colored href="https://github.com/92til-infinity/Project2x4">GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card> */}
 
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-
-                    {/* Project 3
-                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #3</CardTitle>
-                        <CardText>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            </CardText>
-                        <CardActions border>
-                            <Button colored>GitHub</Button>
-
-                        </CardActions>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card> */}
+                    </div>
                 </div>
 
 
             )
         } else if (this.state.activeTab === 1) {
             return (
-                <div><h1>This is Angular</h1></div>
-            )
-        } else if (this.state.activeTab === 2) {
-            return (
-                <div><h1>This is VueJS</h1></div>
-            )
-        } else if (this.state.activeTab === 3) {
-            return (
-                <div><h1>This is MongoDB</h1></div>
-            )
-        } else if (this.state.activeTab === 4) {
-            return (
-                <div><h1>This is Pure HTML/CSS</h1></div>
+                <div><h1>This is Javascript</h1>
+                    <div className="projects-grid">
+                        {/* Project 1 */}
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
+                            <CardText>
+                                Happy Hour Project
+                                    </CardText>
+                            <CardActions border>
+                                <Button colored href="https://92til-infinity.github.io/UCDBC-HappyHour/">GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                        {/* Project 2 */}
+                        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+                            <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #2</CardTitle>
+                            <CardText>
+                                2Play Project
+                                    </CardText>
+                            <CardActions border>
+                                <Button colored href="https://github.com/92til-infinity/Project2x4">GitHub</Button>
+
+                            </CardActions>
+                            <CardMenu style={{ color: '#fff' }}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+
+                    </div>
+                </div>
+
             )
         }
-        else if (this.state.activeTab === 5) {
+        else if (this.state.activeTab === 2) {
             return (
                 <div><h1>This is Python</h1></div>
             )
         }
-        else if (this.state.activeTab === 6) {
+        else if (this.state.activeTab === 3) {
             return (
                 <div><h1>This is Ruby/ On Rails</h1></div>
             )
@@ -97,10 +108,7 @@ class Projects extends Component {
             <div>
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
                     <Tab>React</Tab>
-                    <Tab>Angular</Tab>
-                    <Tab>VueJS</Tab>
-                    <Tab>MongoDB</Tab>
-                    <Tab>Pure HTML/CSS</Tab>
+                    <Tab>Javascript</Tab>
                     <Tab>Python</Tab>
                     <Tab>Ruby/ On Rails</Tab>
                 </Tabs>
